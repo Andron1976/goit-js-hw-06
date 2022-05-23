f1cd4a5b386dade08c5ce0898262999c39e6b2a7
Andron1976 <and.ron@ukr.net>
2022-05-23 18:21:17 +0300

    did task 6

---
+++ js/task-06.js
@@ -0,0 +1,13 @@
+const textInput = document.querySelector('#validation-input');
+const lengthInput = document.querySelector('input[data-length="6"]');
+const focusInput = Number(lengthInput.dataset.length);
+
+textInput.addEventListener('blur', () => {
+if (textInput.value.length === focusInput) {
+textInput.classList.remove('invalid');
+textInput.classList.add('valid');
+} else {
+textInput.classList.remove('valid');
+textInput.classList.add('invalid');
+};
+})
