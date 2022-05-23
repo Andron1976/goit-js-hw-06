77f34121ab7d2e0e7b392a7830544c3070dcba17
Andron1976 <and.ron@ukr.net>
2022-05-23 12:42:33 +0300

    did task 1

--- js/task-01.js
+++ js/task-01.js
@@ -3,10 +3,23 @@
 // Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
 // Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
 // В результате, в консоли будут выведены такие сообщения.
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+const categoriesNumber = document.querySelectorAll('.item');
+console.log(`Number of categories: ${categoriesNumber.length}`);
+
+for (const numb of categoriesNumber) {
+    console.log('Category:', numb.firstElementChild.textContent);
+    console.log('Elements:', numb.querySelectorAll('li').length);
+}
 
 
-
-
-const navItem = document.querySelector('h2');
-
-console.log(navItem);
